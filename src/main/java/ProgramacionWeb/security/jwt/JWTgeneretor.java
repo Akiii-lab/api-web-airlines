f55b6255d56ca51beb97b7480c1f7cce461788db
Akiii-lab <garciasierrandres@gmail.com>
2024-10-25 17:47:16 -0500

    first commit ti security

---
+++ src/main/java/ProgramacionWeb/security/jwt/JWTgeneretor.java
@@ -0,0 +1,5 @@
+package ProgramacionWeb.security.jwt;
+
+public class JWTgeneretor {
+
+}
---
+++ src/main/java/ProgramacionWeb/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,17 @@
+
+package ProgramacionWeb.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+
+        return UserDetailsImpl.build(user);
+    }
+}
